bdb4721fea581638d0d33eb45b67e6c63ebeaf2f
iiTONELOC <anthonytropeano@me.com>
2021-03-06 18:05:35 -0500

    added role table

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,16 @@ CREATE DATABASE employees;
 USE employees;
 
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS role;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL REFERENCES departments(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,38 @@ VALUES
 ('Engineering'),
 ('R&D'),
 ('Marketing'),
-('Sales');
+('Sales'),
+('Facilities'),
+('IT');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Finance Director', 195000, 1),
+('Assistant Finance Director', 159000, 1),
+('Accountant', 61000, 1),
+('Account Assistant', 42750, 1),
+('Lawyer', 94575, 2),
+('Paralegal', 43000, 2),
+('Engineering Director', 165000, 3),
+('Project Leader', 88000,  3),
+('Engineer', 72750, 3),
+('Director of R&D', 189258, 4),
+('R&D Scientist', 91587, 4),
+('R&D Technician', 43654, 4),
+('Brand Planner', 44000, 5),
+('Marketing Operations Manager', 185000, 5),
+('Marketing Specialist', 55000, 5),
+('Business Development Director', 125000, 6),
+('Account Manager', 95000, 6),
+('Account Services', 45000, 6),
+('Maintenance Director', 110000, 7),
+('Maitenance Technican', 52000, 7),
+('Maintenance Helper', 34500, 7),
+('Director of IT', 165000, 8),
+('Network Operations Manager', 120000, 8),
+('Tech Support Technician', 50000, 8),
+("Help Desk", 36000, 8);
+
+
+
+
