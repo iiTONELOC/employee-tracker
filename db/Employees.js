9a4220fb57b77def8d265894a11da8b113581320
iiTONELOC <anthonytropeano@me.com>
2021-03-07 23:27:32 -0500

    added all the questions

--- db/Employees.js
+++ db/Employees.js
@@ -4,7 +4,7 @@ const password = require('../password');
 const cTable = require('console.table');
 const UI = require('../ulits/UI');
 const inquirer = require('inquirer');
-const displayPrompt = require('./QuestionPrompt')
+
 
 class Employees {
     constructor() {
@@ -40,8 +40,7 @@ class Employees {
         con.promise().query(`
                 INSERT INTO employee (first_name, last_name, role_id, manager_id)
                 Values
-                ('${this.first_name}', '${this.last_name}', ${this.role_id}, ${this.manager_id});
-                `)
+                ('${this.first_name}', '${this.last_name}', ${this.role_id}, ${this.manager_id});`)
             .catch(console.log)
             .then(() => con.end());
 
@@ -51,6 +50,8 @@ class Employees {
         let employeeID;
         let roleID;
         let choiceData2;
+        let nameData = [];
+        let roleData = [];
 
         // RETURNS ROLES TO SELECT FROM 
         const con1 = mysql.createConnection(
@@ -61,10 +62,10 @@ class Employees {
                 `)
             .catch(console.log)
             .then(([rows, fields]) => {
-                choiceData2 = rows.map(el => (el.id + ' ' + ' ' + el.title))
+                choiceData2 = rows.map(el => (el.id + ' ' +  el.title))
             })
             .then(() => con1.end());
-
+        // RETURNS THE NAME OF THE EMPLOYEES 
         const con = mysql.createConnection(
             { host: 'localhost', user: 'root', password: password, database: 'employees' }
         );
@@ -73,57 +74,81 @@ class Employees {
                 `)
             .catch(console.log)
             .then(([rows, fields]) => {
-                const choiceData = rows.map(el => (el.id + ' ' + ' ' + el.name))
-                console.log(choiceData)
+                const choiceData = rows.map(el => (el.id + ' ' +  el.name))
+
                 inquirer.prompt([
-                    {
+                    {   // RETURNS THE NAMES OF THE EMPLOYEES IN A LIST 
                         type: 'list',
                         name: 'action',
                         message: 'Select an employee from the list below.',
                         choices: choiceData
                     },
-                    {
+                    {   // RETURNS THE ROLES IN A LIST
                         type: 'list',
                         name: 'action2',
                         message: 'Select a new Role from the list below.',
                         choices: choiceData2,
                         when: ({ action }) => action,
                     },
-                ]).then(({ action, action2 }) => {
+                    {
+                        type: 'list',
+                        name: 'home',
+                        message: 'Return to Main Menu?',
+                        choices: ['Yes', 'No'],
+                        when: ({ action2 }) => action2,
+                    }
+                ]).then(({ action, action2, home }) => {
+                    // GETS EMPLOYEE ID FOR USE IN THE QUERY
                     this.action = action
                     employeeID = this.action.split(' ', 1)
 
-                    this.action2 =action2
-                    roleID = this.action2.split(' ', 1)
-                    if (this.action2){
-                        console.log (roleID)
-                    }
+                    // STORES NAME AND ID TO DISPLAY LATER IN A MESSAGE
+                    nameData.push(this.action)
 
+                    // STORES THE ROLE ID FOR USE IN THE QUERY
+                    this.action2 = action2
+                    roleID = this.action2.split(' ', 1)
+
+                    // STORES THE ROLE DESCRIPTION TO DISPLAY LATER IN A MESSAGE
+                    roleData.push(this.action2)
+
+                    this.home = home
+
+                    // WHEN THE ROLE HAS BEEN SELECTED THE QUERY IS RUN TO UPDATE THE DATABASE
+                    if (this.action2) {
+                        // console.log (`The role for ${name} has been updated to ${this.action2}`)
+                        const con2 = mysql.createConnection(
+                            { host: 'localhost', user: 'root', password: password, database: 'employees' }
+                        );
+
+                        con2.promise().query(`
+                        UPDATE employee SET role_id = ${roleID}
+                        WHERE id = ${employeeID};
+                                `)
+                            .then(() => {
+                                new UI().displaySingBreak();
+                                console.log(`Success! Employee # ${nameData}'s Role was updated to role_id: ${roleData}`)
+                            })
+                            .catch(console.log)
+                            .then(() => con2.end())
+                    }
+                    // WHEN FINISHED YOU CAN EDIT MORE EMPLOYEES OR RETURN TO MAIN 
+                    if (this.home) {
+                        if (this.home === 'Yes') {
+                            const displayPrompt = require('./QuestionPrompt')
+                            displayPrompt()
+                        } else {
+                            new Employees().initiateEmployeeUpdate();
+                        }
+                    }
                 })
             })
-            .then(() => con.end())
+            .then(() => con.end());
     }
 }
 
-// ]).then(({ action2 }) => {
-//     this.action2 = action2
-//     console.log(this.action2)
-//     roleID = this.action2.split(' ', 1)
-//     console.log(roleID)
 
-// })
-// inquirer.prompt([
-//     {
-//         type: 'list',
-//         name: 'action2',
-//         message: 'Select an employee from the list below.',
-//         choices: choiceData
-//     },
-// ]).then(({ action2 }) => {
-//     this.action2 = action2
-//     employeeID = this.action.split(' ', 1)
-// })
-
+//
 
 
 module.exports = Employees;
