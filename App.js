92fc6ac7c7d09cc859f38993e3c07e24d3d413d7
iiTONELOC <anthonytropeano@me.com>
2021-03-10 04:00:23 -0500

    accepting the idea I can't launch the sql files through the app

--- App.js
+++ App.js
@@ -31,30 +31,11 @@ class App {
         questionPrompt()
 
     }
+
     initializeDatabase() {
-    //console.clear();
-    const { execSync } = require("child_process");
-
-// execute touch command synchronously
-// to create three text file
-const stdout = execSync("mysql employees");
-
-console.log(stdout); // stdout of the command if any
-
+        console.clear()  
     }
-    loadDatabase() {
-        const con2 = mysql.createConnection(
-            { host: 'localhost', user: 'root', password: password, database: 'employees' }
-        );
-        con2.promise().query(`
-        source db/schema.sql;`)
-            .then(() => {
-                console.log(`\nSuccess! \nEmployee # ${nameData}'s role has been updated to role_id: ${roleData}`)
-            })
-            .catch(console.log)
-            .then(() => con2.end())
 
-    }
     viewInstructions() {
         console.log(`\n${instructions}`)
     }
